d5bc7b8a5da2b6704d3f88f0c173110d104799fc
avivpaz <avivpaz43556@gmail.com>
2024-12-27 22:40:14 +0200

    💄 style(page): adjust formatting and indentation in page.js
    - enhance code readability with consistent indentation
    - apply uniform spacing for better visual structure
    
    ✨ feat(app): add robots.txt configuration
    
    - introduce new function to configure robots.txt rules
    - allow facebookexternalhit agent access and general access to all
    - specify sitemap location in robots.txt response

--- src/app/[organizationId]/jobs/[id]/page.js
+++ src/app/[organizationId]/jobs/[id]/page.js
@@ -1,54 +1,54 @@
-// src/app/[organizationId]/jobs/[id]/page.js
-import JobApplication from '../../../components/JobApplication';
-import ApiService from '@/app/services/ApiService';
+  // src/app/[organizationId]/jobs/[id]/page.js
+  import JobApplication from '../../../components/JobApplication';
+  import ApiService from '@/app/services/ApiService';
 
-async function getJobData(params) {
-  const { organizationId, id } =await params;
-  
-  try {
-    const [organizationDetails, jobDetails] = await Promise.all([
-      ApiService.getServerSideApi(`/organizations/${organizationId}`),
-      ApiService.getServerSideApi(`/jobs/${id}`)
-    ]);
+  async function getJobData(params) {
+    const { organizationId, id } =await params;
+    
+    try {
+      const [organizationDetails, jobDetails] = await Promise.all([
+        ApiService.getServerSideApi(`/organizations/${organizationId}`),
+        ApiService.getServerSideApi(`/jobs/${id}`)
+      ]);
 
-    return { organizationDetails, jobDetails };
-  } catch (error) {
-    console.error('Error fetching details:', error);
-    throw error;
+      return { organizationDetails, jobDetails };
+    } catch (error) {
+      console.error('Error fetching details:', error);
+      throw error;
+    }
   }
-}
 
-export async function generateMetadata({ params }) {
-  const { organizationDetails, jobDetails } = await getJobData(params);
+  export async function generateMetadata({ params }) {
+    const { organizationDetails, jobDetails } = await getJobData(params);
 
-  return {
-    title: `${jobDetails.title} at ${organizationDetails.name}`,
-    description: jobDetails.description,
-    openGraph: {
-      title: `${jobDetails.title} at ${organizationDetails.name}`,
-      description: `${jobDetails.workType} ${jobDetails.employmentType} position in ${jobDetails.location}. Required Skills: ${jobDetails.requiredSkills.join(', ')}`,
-      images: [
-        {
-          url: organizationDetails.logoUrl || '/og-image.jpg',
-          width: 1200,
-          height: 630,
-          alt: organizationDetails.name
-        }
-      ],
-      siteName: organizationDetails.name,
-      type: 'website',
-      locale: 'en_US',
-    },
-    twitter: {
-      card: 'summary_large_image',
+    return {
       title: `${jobDetails.title} at ${organizationDetails.name}`,
       description: jobDetails.description,
-      images: [organizationDetails.logoUrl || '/og-image.jpg'],
-    }
-  };
-}
+      openGraph: {
+        title: `${jobDetails.title} at ${organizationDetails.name}`,
+        description: `${jobDetails.workType} ${jobDetails.employmentType} position in ${jobDetails.location}. Required Skills: ${jobDetails.requiredSkills.join(', ')}`,
+        images: [
+          {
+            url: organizationDetails.logoUrl || '/og-image.jpg',
+            width: 1200,
+            height: 630,
+            alt: organizationDetails.name
+          }
+        ],
+        siteName: organizationDetails.name,
+        type: 'website',
+        locale: 'en_US',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: `${jobDetails.title} at ${organizationDetails.name}`,
+        description: jobDetails.description,
+        images: [organizationDetails.logoUrl || '/og-image.jpg'],
+      }
+    };
+  }
 
-export default async function Page({ params }) {
-  const initialData = await getJobData(params);
-  return <JobApplication params={params} initialData={initialData} />;
-}
+  export default async function Page({ params }) {
+    const initialData = await getJobData(params);
+    return <JobApplication params={params} initialData={initialData} />;
+  }
